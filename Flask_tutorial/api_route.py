58e5f01826495eb6d41e480204b25775bde89dea
unknown <khitis222@gmail.com>
2026-08-16 16:34:19 +0530

    added api_route file

---
+++ Flask_tutorial/api_route.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import json
+
+app = Flask(__name__)
+
+@app.route('/api')
+
+def get_data():
+
+    with open ("Backend/data.json", "r") as f:
+        data = json.load(f)
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
